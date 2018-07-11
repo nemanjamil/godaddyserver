18f842ee20628725a22e6da762e0aa3fe8e0a5a9
nemanjamil <nemanjamil@gmail.com>
2018-07-11 15:38:52 +0200

    Routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['prefix' => '1.0'], function () {
+    Route::get('all', "NotesController@index");
+    Route::post('addnote', "NotesController@store");
+});
+
