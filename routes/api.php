939f8d1866fb1015d9733ccf8ab5b00813ad00da
nemanjamil <nemanjamil@gmail.com>
2018-07-18 14:05:15 +0200

    Update 18_7_2018

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::group(['prefix' => '1.0'], function () {
 
 
 Route::group(['prefix' => '1.0','middleware' => 'auth:api'], function () {
-    Route::get('all', "NotesController@index");
+    //Route::get('all', "NotesController@index");
     Route::get('note/{id}', "NotesController@show");
     Route::post('addnote', "NotesController@store");
     Route::post('updatenote/{id}', "NotesController@update");
